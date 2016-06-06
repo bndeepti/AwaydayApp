336a440f1240b6dad492e4fadd58012e95ae1c2d
Pankaj <ladharpankaj@gmail.com>
2016-06-06 17:10:12 +0530

    Pankaj | Added new project skeleton

--- server/server.js
+++ server/server.js
@@ -1,14 +1,51 @@
-const http = require('http');
+var express = require("express");
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var app = express();
 
-const server = http.createServer(function(req, res){
-      res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-          res.end('Hello World\n');
+app.use(express.static(path.join(__dirname,'..', 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve("index.html"));
 });
 
-server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+ 
+// Start the server
+app.set('port', process.env.PORT || 3000);
+ 
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
 });
