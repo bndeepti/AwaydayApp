da49a3fa80a9c9a30f66b64d56d7824772180e14
Archanaa Ravikumar <archanar@thoughtworks.com>
2016-06-07 13:39:19 +0530

    Creating views folder to hold the html files <Archanaa>

--- server/server.js
+++ server/server.js
@@ -5,35 +5,10 @@ var bodyParser = require('body-parser');
 var router = require('./routes/routes');
 var app = express();
 
-app.use(express.static(path.join(__dirname,'..', 'public')));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.resolve("index.html"));
-});
+app.set('public', path.join(__dirname, '../public'));
+app.set('views', path.join(__dirname, '../views'));
 app.use("/",router);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.sendFile(path.resolve("error.html"));
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.sendFile(path.resolve("index.html"));
-});
- 
 // Start the server
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
