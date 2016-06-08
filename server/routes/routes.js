7a7ca86bada904edd96c146e2c335f8e1a2a6c86
Archanaa Ravikumar <archanar@thoughtworks.com>
2016-06-08 12:19:29 +0530

    Cleanup - moving html files to a view folder <Archanaa>

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var authentication = require('./authentication').router;
+var home = require('./home').router;
 var router = express.Router();
+
+router.use("/", home);
 router.use("/login",authentication);
 module.exports=router;
--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var router = require('./routes/routes');
+var html = require('html');
 var app = express();
 
-app.set('public', path.join(__dirname, '../public'));
-app.set('views', path.join(__dirname, '../views'));
-app.use("/",router);
+app.set('public', path.join(__dirname,'..','public'));
+app.set('views', path.join(__dirname,'..','views'));
 
+app.use("/",router);
+ 
 // Start the server
 app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'html');
+app.set('view engine', html); 
+
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
